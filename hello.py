326db72fe73229d3c183191e0b0b6446b2d68dfb
Jason-Du <53274895+Jason-Du@users.noreply.github.com>
2020-09-01 14:07:53 +0800

    local master change 0901_0154

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 print('hello python for master  local checkout commit ')
 print('online chane 0901')
+=======
+print('hello python for master  local  change in 0901/0154 ')
+
+>>>>>>> cff5997... local master change 0901_0154
