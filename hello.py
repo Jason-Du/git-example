983c4ac190c60e75083d84a35c3d2b2b1571ebc4
Jason-Du <53274895+Jason-Du@users.noreply.github.com>
2020-09-01 14:09:58 +0800

    local master change 0901_0154 finish conflict

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-print('hello python for master  local checkout commit ')
-print('online chane 0901')
-=======
 print('hello python for master  local  change in 0901/0154 ')
-
->>>>>>> cff5997... local master change 0901_0154
